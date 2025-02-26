10c0d81e701c611e5e37510fcef264ee80c69a14
YangYang <1059376437@qq.com>
2025-02-26 22:50:48 +0800

    chore: 移除旧的 ESLint 配置，添加 @antfu/eslint-config 以支持 TypeScript 和 Vue。Fixes #1

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // 继承的规则集
-    extends: [
-        '@antfu' // 继承 antfu 的 ESLint 规则
-    ],
-};
---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
+import parserTs from '@typescript-eslint/parser' // 需额外安装
+
+export default [
+ ...antfu({
+   typescript: true,           // 启用 TS 支持
+   vue: true,                  // 启用 Vue 支持
+   parserOptions: {
+     parser: parserTs          // 指定 TS 解析器（参考内容提到 parser 配置）
+   }
+ })
+]
