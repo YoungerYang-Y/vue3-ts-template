cc0b2b56dfc8bc1459ae61088fcca02b743f0334
YangYang <1059376437@qq.com>
2025-03-16 01:57:33 +0800

    build: add Vue Router integration with automatic route generation and create About and Index pages

---
+++ src/pages/about.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>About</h1>
+  </div>
+  <RouterLink to="/">
+    <button>
+      Go back index
+    </button>
+  </RouterLink>
+</template>
+
+<style scoped>
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    // https://github.com/posva/unplugin-vue-router
+    VueRouter({
+      dts: 'src/typed-router.d.ts',
+    }),
+
+    // https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue
     vue(),
+
+    // https://github.com/antfu/unplugin-vue-components
     Components({
       dts: 'src/components.d.ts',
     }),
+
+    // https://github.com/antfu/unplugin-auto-import
     AutoImport({
       imports: [
         'vue',
+        VueRouterAutoImports,
       ],
       dts: 'src/auto-imports.d.ts',
       vueTemplate: true, // 允许在 <template> 直接使用自动导入的 API
