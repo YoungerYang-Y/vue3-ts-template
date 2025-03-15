a46ba2fa5dd0898a75ce3b23d34baef9fc631bdf
YangYang <1059376437@qq.com>
2025-03-16 00:31:18 +0800

    build: integrate unplugin-auto-import for automatic API imports and update HelloWorld component

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({
-    dts: 'src/components.d.ts',
-  })],
+  plugins: [
+    vue(),
+    Components({
+      dts: 'src/components.d.ts',
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      vueTemplate: true, // 允许在 <template> 直接使用自动导入的 API
+    }),
+  ],
 })
