3b8f3dfaaf47fde5aac9824cc99ccbb899594b50
YangYang <1059376437@qq.com>
2025-09-19 22:14:44 +0800

    feat: 删除 HelloWorld 组件，更新首页布局，添加导航至仪表盘

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,18 +2,14 @@
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo">
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo">
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
   <RouterLink to="/about">
     <n-button>
       Go to about
+    </n-button>
+  </RouterLink>
+  <RouterLink to="/dashboard">
+    <n-button>
+      Go to dashboard
     </n-button>
   </RouterLink>
 </template>
